e603ad3b0751eed6ae7ba8bb9f166f8f4747f4d6
shuos-rst <54552425+shuos-rst@users.noreply.github.com>
2021-03-09 16:06:19 -0600

    more initial changes

---
+++ polonium.py
@@ -0,0 +1,25 @@
+import os
+import discord
+    #https://discordpy.readthedocs.io/en/latest/index.html#
+from dotenv import load_dotenv #this is a thing for the discord token. 
+    #https://realpython.com/how-to-make-a-discord-bot-python/#
+    #https://pypi.org/project/python-dotenv/#
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('we have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user():
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN) #it gets the token from the .env file which we interface with using dotenv
