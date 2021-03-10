e2d4f961da65d01f3d74fe8363ed26ed77325770
shuos-rst <54552425+shuos-rst@users.noreply.github.com>
2021-03-09 20:28:41 -0600

    the vibes have been cured

--- dice.py
+++ dice.py
@@ -1,6 +1,5 @@
-import random
 import numpy as np
-from numpy.lib.twodim_base import mask_indices
+import re
 
 #todo get the dice code working with polonium.py
 
@@ -8,35 +7,24 @@ class InputError(Exception):
     pass
 
 def dn(n): #a die with n sides
-    return np.random.randr(1, n+1)
+    return np.random.randint(1,n+1,1)
 
 def xdn(x, n): #x dice with n sides
-    if x < 1 or n < 1:
-        raise InvalidError()
+    if (x < 1) or (n < 1):
+        return("numprob")
     dice = np.random.randint(1,n+1,x)
     total = np.sum(dice)
-    # for i in range(x):
-    #     ret = dn(n)
-    #     total = total + ret
-    #     dice.append(ret)
-    # string_ints = [str(int) for int in dice] #this creates a new variable string_ints, and initializes it by taking every int in dice and converting it to a string
-    return("Sum: " + str(total) + ". Dice: " + ", ".join(dice.tolist()))
+    return("sum: " + str(total) + "\n" + np.array2string(dice, separator=','))
     
 def dice_roll(discord_input):
-    try:
         diceInput = discord_input.partition("d") #returns a tuple with index 0 being the x, index 1 being d, index 2 being the n
-        if (diceInput[0] == discord_input): #checks for a failure to partition caused by nonexistent "d"
-            raise InputError()
+        if not re.search(r'^\d*?d\d+$',discord_input):
+            return "the vibes are simply rancid"
         n = int(diceInput[2])
         if (not diceInput[0]):
             print(dn(n))
         else:
             x = int(diceInput[0])
             return (xdn(x,n))
-    except:
-        return ("Invalid Input.")
-#todo: implement edge case handling
-    #5. wayy too many dice
-    #6. wayy too many dice rolls
 
 
