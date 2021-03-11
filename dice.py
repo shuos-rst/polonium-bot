b90d7e4e4ef8c6e428ddfe1fc180bdf876294b4a
shuos-rst <54552425+shuos-rst@users.noreply.github.com>
2021-03-11 00:52:09 -0600

    added comments to dice.py, randimage.py, and a couple more mikus

--- dice.py
+++ dice.py
@@ -12,23 +12,23 @@ def dn(n): #a die with n sides
 def xdn(x, n): #x dice with n sides
     if (x < 1) or (n < 1):
         return("numprob")
-    dice = np.random.randint(1,n+1,x)
+    dice = np.random.randint(1,n+1,x) #this returns an array
     total = np.sum(dice)
     return("sum: " + str(total) + "\n" + np.array2string(dice, separator=','))
 
 def xdn_ad(x, n): #x dice with n sides, advantage
     if (x < 1) or (n < 1):
         return("numprob")
-    dice = np.random.randint(1,n+1,x)
+    dice = np.random.randint(1,n+1,x) #this returns an array
     total = np.sum(dice)
-    return("sum: " + str(total) + "\n" + np.array2string(dice, separator=',') + "\n" + "max: " + np.array2string(np.amax(dice)))
+    return("sum: " + str(total) + "\n" + np.array2string(dice, separator=',') + "\n" + "max: " + np.array2string(np.amax(dice))) #np.amax gets the max of the array, but returns that as an array, so we have to convert it to a string
 
 def xdn_da(x, n): #x dice with n sides, disadvantage
     if (x < 1) or (n < 1):
         return("numprob")
-    dice = np.random.randint(1,n+1,x)
+    dice = np.random.randint(1,n+1,x) #this returns an array
     total = np.sum(dice)
-    return("sum: " + str(total) + "\n" + np.array2string(dice, separator=',') + "\n" + "min: " + np.array2string(np.amin(dice)))
+    return("sum: " + str(total) + "\n" + np.array2string(dice, separator=',') + "\n" + "min: " + np.array2string(np.amin(dice))) #np.amin gets the min of the array, but returns that as an array, so we have to convert it to a string
     
 def dice_roll(discord_input, type):
         diceInput = discord_input.partition("d") #returns a tuple with index 0 being the x, index 1 being d, index 2 being the n
--- randimage.py
+++ randimage.py
@@ -7,12 +7,12 @@ class randimage():
 
     images = set()
 
-    def __init__(self, file):                
+    def __init__(self, file): #constructor                
         self.images = set()
-        with open(file, newline='') as file_input: #this bit of code should read in lines from the csv of wooper images
+        with open(file, newline='') as file_input: #this bit of code should read in lines from the csv of images
             file_reader = csv.reader(file_input)
             for row in file_reader:
-                self.images.add(str(row))
+                self.images.add(str(row)) #we store the images in a set because .pop() from a set is random access
 
 
     def rand(self):
