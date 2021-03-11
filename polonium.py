63bf04222bf65cbeb5bd9b29058d02667b3738ce
shuos-rst <54552425+shuos-rst@users.noreply.github.com>
2021-03-11 16:17:34 -0600

    got rid of bad comments

--- polonium.py
+++ polonium.py
@@ -35,9 +35,9 @@ client = discord.Client()
 
 
 
-#info about the async/await syntax: https://realpython.com/async-io-python/#
-
 #async code that executes on a particular event
+    #info about the async/await syntax: https://realpython.com/async-io-python/#
+
 @client.event
 async def on_ready(): #prints when the bot connects/reconnects to discord
     print('we have logged in as {0.user}'.format(client))
@@ -53,16 +53,3 @@ async def on_message(message):
         await message.channel.send(cmnd.find_and_execute(messageInput))
 
 client.run(TOKEN)
-
-#TODO is there a better way to handle keyword -> code then this structure? I need to check my 332S book
-    #command pattern -- #https://www.notion.so/shuos/Command-Pattern-595ac1a1e9c842b0a0cdff68e948acfa#
-
-    #ok so. here's how its gonna go down
-    #have a command prompt class, with a dictionary with keys being the command, and values being a pointer to the instance of each command
-    #have an abstract base class that all command.py files are derived from.
-    #have all the command.py files be a class with a constructor and an execute method that returns a string
-    #register all command objects to the command prompt like in lab5
-        #cp.addCommand("ls", new LSCommand(sfs));# from lab5
-
-    #when a command is entered, we first check if it's got the KEYWORD and not sent by us.
-    #then we look it up in the dictionary and execute the command from that pointer.
